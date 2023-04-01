49b75cc0672a109091ef6bbfc87ef9f136419c70
JoaquinPettinari <joaquinpettinari@hotmail.com>
2023-04-01 20:06:09 -0300

    Change config of eslint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import { memo } from 'react'
 import { Grid, Typography } from '@mui/material'
 
-function Home(){
-    return(
-        <Grid container>
-            <Typography color={"primary"}>
-                Primary
-            </Typography>
-        </Grid>
-    )
+function Home () {
+  return (
+    <Grid container>
+      <Typography color={'primary'}>Primary mui</Typography>
+    </Grid>
+  )
 }
 
 export default memo(Home)
